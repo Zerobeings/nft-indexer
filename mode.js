525c0db05c882821ae3665a3c38f557f38e0c350
CalculatingZero <20590197+CalculatingZero@users.noreply.github.com>
2023-12-24 00:38:48 -0800

    add font awesome icons

--- mode.js
+++ mode.js
@@ -11,3 +11,11 @@ darkModeToggle.addEventListener('click', () => {
 // Initialize the correct icon
 iconSun.classList.toggle('hidden', document.body.classList.contains('dark-mode'));
 iconMoon.classList.toggle('hidden', !document.body.classList.contains('dark-mode'));
+
+// Inititialize with darkmode and hide the lightmode icon
+document.addEventListener('DOMContentLoaded', () => {
+    document.body.classList.toggle('dark-mode');
+    iconSun.classList.toggle('hidden');
+    iconMoon.classList.toggle('hidden');
+}
+);
