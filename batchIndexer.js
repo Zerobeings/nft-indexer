8e94fa346bc06332125721a2c4ffaf3baa757ed1
CalculatingZero <20590197+CalculatingZero@users.noreply.github.com>
2024-01-03 18:25:34 -0800

    updating indexing scripts

--- batchIndexer.js
+++ batchIndexer.js
@@ -1,3 +1,5 @@
+// Please note this script is not tested yet and is still in development
+
 const Mixtape = require('mixtapejs');
 const fetch = require('cross-fetch');
 const axios = require('axios');
@@ -12,15 +14,38 @@ const mixtape = new Mixtape();
 require('dotenv').config();
 const ethDirectory = require('./ethDirectory');
 const polyDirectory = require('./polyDirectory');
+const avaxDirectory = require('./avaxDirectory');
+const ftmDirectory = require('./ftmDirectory');
 
 async function updateIndexedCollections(contractAddress, network){
   try {
       // Determine the correct directory based on the network
-      const dirPath = network === 'ethereum' ? 'eth-indexed' : 'poly-indexed';
+      let dirPath;
+
+      switch (network) {
+        case 'ethereum':
+          dirPath = 'eth-indexed';
+          break;
+        case 'polygon':
+          dirPath = 'poly-indexed';
+          break;
+        case 'avalanche':
+          dirPath = 'avax-indexed';
+          break;
+        case 'fantom':
+          dirPath = 'ftm-indexed';
+          break;
+        default:
+          throw new Error(`Unsupported network: ${network}`);
+      }
+
       const filePath = path.join(__dirname, dirPath, 'indexed.json');
 
       // Initialize indexedCollections
       let indexedCollections = {};
+
+      // Batch size for fetching tokens
+      const batchSize = 100;
 
       // Check if the file exists and read the existing data
       if (fs.existsSync(filePath)) {
@@ -83,30 +108,29 @@ const getIPFSUrl = (url) => {
   return `${gateway}${CID}`;
 };
 
-const getContractURI = async (contractAddress, tokenId, provider,
-   //providerI, 
-   network) => {
-    // let abiSpecial;
+const getContractURI = async (contractAddress, tokenId, provider, network) => {
+    let abiSpecial;
+    let providerI;
     console.log(network)
+    // todo: add other networks and confirm contract calls for those networks
     if (network === 'ethereum') {
       const contract = new ethers.Contract(contractAddress, [
         'function tokenURI(uint256 tokenId) view returns (string)'
       ], provider);
-
-      // abiSpecial = [
-      //   'function tokenURI(uint256 tokenId) view returns (string)'
-      // ];
-      // const contractSpecial = new ethers.Contract(contractAddress, abiSpecial, providerI);
-
       try {
         return await contract.tokenURI(tokenId);
       } catch (error) {
         console.error(`Error fetching tokenURI for ${contractAddress} and ${tokenId}: ${error.message}`);
-        // try {
-        //   return await contractSpecial.tokenURI(tokenId);
-        // } catch (error) {
-        //   console.error(`Error fetching tokenURI for ${contractAddress} and ${tokenId}: ${error.message}`);
-        // }
+        try {
+          abiSpecial = [
+            'function tokenURI(uint256 tokenId) view returns (string)'
+          ];
+          const contractSpecial = new ethers.Contract(contractAddress, abiSpecial, providerI);
+
+          return await contractSpecial.tokenURI(tokenId);
+        } catch (error) {
+          console.error(`Error fetching tokenURI for ${contractAddress} and ${tokenId}: ${error.message}`);
+        }
       }
 
   } else {
@@ -140,50 +164,13 @@ const parseDataUri = (dataUri) => {
   }
 };
 
-const createMixtapeForContract = async ( contractAddress, startToken, endToken, network ) => {
-  console.log(`Creating mixtape for ${contractAddress}...${network}`);
-
-  let provider;
-  // let providerI;
-
-  switch (network) {
-    case 'polygon':
-      provider = new ethers.JsonRpcProvider('https://polygon.rpc.thirdweb.com');
-      break;
-    case 'ethereum':
-      provider = new ethers.JsonRpcProvider('https://ethereum.rpc.thirdweb.com');
-      // providerI = new ethers.JsonRpcProvider(process.env.INFURA_URL);
-      break;
-    default:
-      throw new Error(`Unsupported network: ${network}`);
-  }
-  
-    const networkDirPath = path.join(__dirname, network);
-    if (!fs.existsSync(networkDirPath)) {
-        fs.mkdirSync(networkDirPath);
-    }
-  
-    const dirPath = path.join(networkDirPath, contractAddress);
-    if (!fs.existsSync(dirPath)){
-        fs.mkdirSync(dirPath);
-    }
-
-  await mixtape.init({
-    path: path.join(__dirname, network, contractAddress),
-    config: {
-      metadata: { schema: "migrate" }
-    }
-  });
-
-  for (let tokenId = startToken; tokenId <= endToken; tokenId++) {
+  const fetchTokenMetadata = async (contractAddress, tokenId, provider, network) => {
     let success = false;
     let attempts = 0;
     let metadata;
     while (!success && attempts < 15) {
         try {
-            const uri = await getContractURI(contractAddress, tokenId, provider,
-               //providerI, 
-               network);
+            const uri = await getContractURI(contractAddress, tokenId, provider, network);
             const fetchURI = isIPFS(uri) ? getIPFSUrl(uri) : uri;
             console.log(`Fetching metadata for token ${tokenId}`);
 
@@ -215,48 +202,96 @@ const createMixtapeForContract = async ( contractAddress, startToken, endToken, 
                 }     
               }
             } else {
+              try {
+                const responseWithExtension = await fetchWithTimeout(`${fetchURI}/${tokenId}.json`);
+                metadata = await responseWithExtension.json();
+              } catch (errorWithExtension) {
+                console.error(`Error fetching with .json extension: ${errorWithExtension.message}`);
                 try {
-                  const responseWithExtension = await fetchWithTimeout(`${fetchURI}/${tokenId}.json`);
-                  metadata = await responseWithExtension.json();
-                } catch (errorWithExtension) {
-                  console.error(`Error fetching with .json extension: ${errorWithExtension.message}`);
-                  try {
-                    const responseWithoutExtension = await fetchWithTimeout(`${fetchURI}/${tokenId}`);
-                    const responseText = await responseWithoutExtension.text();
-                    metadata = JSON.parse(responseText); // Parse the response text into JSON
-                  } catch (errorWithoutExtension) {
-                    attempts++;
-                    console.error(`Error fetching without .json extension: ${errorWithoutExtension.message}`);
-                  }
+                  const responseWithoutExtension = await fetchWithTimeout(`${fetchURI}/${tokenId}`);
+                  const responseText = await responseWithoutExtension.text();
+                  metadata = JSON.parse(responseText); // Parse the response text into JSON
+                } catch (errorWithoutExtension) {
+                  attempts++;
+                  console.error(`Error fetching without .json extension: ${errorWithoutExtension.message}`);
                 }
+              }
             }
 
-            if (metadata) {
-                metadata.index = tokenId; // Add index to metadata
-                await mixtape.write("metadata", metadata);
-                success = true;
-                console.log(`Fetched and processed token ${tokenId}`);
-            }
         } catch (error) {
-            attempts++;
-            console.log(`Attempt ${attempts} failed for token ${tokenId}: ${error.message}`);
-            if (error.message.includes('revert') || error.message.includes('nonexistent token')) {
-                console.error(`Token ${tokenId} not found, stopping.`);
-                break;
-            }
-            
-            if (attempts >= 15) {
-                console.error(`Failed to fetch metadata for token ${tokenId} after multiple attempts, stopping.`);
-                break;
+          attempts++;
+          console.error(`Attempt ${attempts} failed for item ${tokenId}: ${error.message}`);
+          if (attempts >= 15) {
+            throw new Error(`Failed to fetch metadata for item ${tokenId} after multiple attempts`);
+          }
+        }
+    }
+  }
+
+
+const createMixtapeForContract = async ( contractAddress, startToken, endToken, network ) => {
+  console.log(`Creating mixtape for ${contractAddress}...${network}`);
+
+  let provider;
+
+  switch (network) {
+    case 'polygon':
+      provider = new ethers.JsonRpcProvider('https://polygon.rpc.thirdweb.com');
+      break;
+    case 'ethereum':
+      provider = new ethers.JsonRpcProvider('https://ethereum.rpc.thirdweb.com');
+      break;
+    case 'avalanche':
+      provider = new ethers.JsonRpcProvider('https://avalanche.rpc.thirdweb.com');
+      break;
+    case 'fantom':
+      provider = new ethers.JsonRpcProvider('https://fantom.rpc.thirdweb.com');
+      break;
+    default:
+      throw new Error(`Unsupported network: ${network}`);
+  }
+  
+    const networkDirPath = path.join(__dirname, network);
+    if (!fs.existsSync(networkDirPath)) {
+        fs.mkdirSync(networkDirPath);
+    }
+  
+    const dirPath = path.join(networkDirPath, contractAddress);
+    if (!fs.existsSync(dirPath)){
+        fs.mkdirSync(dirPath);
+    }
+
+  await mixtape.init({
+    path: path.join(__dirname, network, contractAddress),
+    config: {
+      metadata: { schema: "migrate" }
+    }
+  });
+
+  for (let batchStart = startToken; batchStart <= endToken; batchStart += batchSize) {
+    const batchEnd = Math.min(batchStart + batchSize - 1, endToken);
+    const batchPromises = [];
+
+    for (let tokenId = batchStart; tokenId <= batchEnd; tokenId++) {
+        batchPromises.push(fetchTokenMetadata(contractAddress, tokenId, provider, network));
+    }
+
+    try {
+        const batchResults = await Promise.all(batchPromises);
+        for (const metadata of batchResults) {
+            if (metadata) {
+                await mixtape.write("metadata", metadata);
             }
         }
+    } catch (error) {
+        console.error(`Error in batch ${batchStart}-${batchEnd}: ${error.message}`);
     }
   }
 
   console.log(`Finished fetching all tokens for ${contractAddress}.`);
 };
 
-const delay = 15 * 60 * 1000; // 15 minutes in milliseconds
+const delay = 1 * 60 * 1000; // 1 minutes in milliseconds
 
 
 const pushToGitHub = (network) => {
@@ -305,15 +340,8 @@ const runScriptForNetwork = async (network) => {
     }
     // After mixtape creation is complete
     if (changesMade) {
-        // Push to GitHub
-        // try {
-        //   await pushToGitHub(network);
-        // } catch (error) {
-        //   console.error(`Error pushing to GitHub: ${error.message}`);
-        // }
-
-        // Update directory
-        if (network === 'ethereum') {
+         // Update directory
+         if (network === 'ethereum') {
           try {
             await ethDirectory.fetchAllMetadata();
           } catch (error) {
@@ -325,15 +353,51 @@ const runScriptForNetwork = async (network) => {
           } catch (error) {
             console.error(`Error writing metadata for ${contractAddress}: ${error.message}`);
           }
+        } else if (network === 'avalanche') {
+          try {
+            await avaxDirectory.fetchAllMetadata();
+          } catch (error) {
+            console.error(`Error writing metadata for ${contractAddress}: ${error.message}`);
+          }
+        }
+        else if (network === 'fantom') {
+          try {
+            await ftmDirectory.fetchAllMetadata();
+          } catch (error) {
+            console.error(`Error writing metadata for ${contractAddress}: ${error.message}`);
+          }
+        }
+
+        //Push to GitHub
+        try {
+          await pushToGitHub(network);
+        } catch (error) {
+          console.error(`Error pushing to GitHub: ${error.message}`);
         }
     } else {
         console.log(`No changes made for ${network} network.`);
     }
 };
 
-// Run for Ethereum, then wait 15 minutes and run for Polygon
-runScriptForNetwork('ethereum').then(() => {
-    setTimeout(() => runScriptForNetwork('polygon'), delay);
-});
+// Run for all networks in a loop
+function runScriptsInLoop() {
+  runScriptForNetwork('ethereum').then(() => {
+      setTimeout(() => {
+          runScriptForNetwork('polygon').then(() => {
+              setTimeout(() => {
+                  runScriptForNetwork('avalanche').then(() => {
+                      setTimeout(() => {
+                          runScriptForNetwork('fantom').then(() => {
+                              setTimeout(runScriptsInLoop, delay);
+                          });
+                      }, delay);
+                  });
+              }, delay);
+          });
+      }, delay);
+  });
+}
+
+runScriptsInLoop();
 
 module.exports = { updateIndexedCollections, fetchDataFromRequest, runScriptForNetwork };
